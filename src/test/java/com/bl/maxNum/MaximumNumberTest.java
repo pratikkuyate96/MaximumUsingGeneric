447c9221a9a2e770d6a5afe9ef0a748826810c49
pratikkuyate96 <62277306+pratikkuyate96@users.noreply.github.com>
2020-04-28 17:33:32 +0530

    ADDED: Print maximum value using generic

--- src/test/java/com/bl/maxNum/MaximumNumberTest.java
+++ src/test/java/com/bl/maxNum/MaximumNumberTest.java
@@ -4,21 +4,25 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MaximumNumberTest {
+
     MaximumNumber maximumNumber = new MaximumNumber();
 
     @Test
     public void givenThreeIntegers_findMaximum_atFirstPosition() {
-        Assert.assertEquals(30,maximumNumber.findMaximum(30,30,10));
+        Comparable maximum=maximumNumber.findMaximum(30,20,10);
+        Assert.assertEquals(30,maximum);
     }
 
     @Test
     public void givenThreeIntegers_findMaximum_atSecondPosition() {
-        Assert.assertEquals(30,maximumNumber.findMaximum(10,30,20));
+        Comparable maximum=maximumNumber.findMaximum(10,30,20);
+        Assert.assertEquals(30,maximum);
     }
 
     @Test
     public void givenThreeIntegers_findMaximum_atThirdPosition() {
-        Assert.assertEquals(30,maximumNumber.findMaximum(10,20,30));
+        Comparable maximum=maximumNumber.findMaximum(10,20,30);
+        Assert.assertEquals(30,maximum);
     }
 
     @Test
@@ -32,7 +36,7 @@ public class MaximumNumberTest {
         Comparable maximum=maximumNumber.findMaximum(10.2f,30.1f,20.1f);
         Assert.assertEquals(30.1f,maximum);
     }
-    
+
     @Test
     public void givenThreeFloat_findMaximum_atThirdPosition() {
         Comparable maximum =maximumNumber.findMaximum(10.1f,20.1f,30.1f);
@@ -57,4 +61,21 @@ public class MaximumNumberTest {
         Assert.assertEquals((String) "Capgiminai",maximum);
     }
 
+    @Test
+    public void givenThreeInteger_shouldReturnMaximumInteger() {
+        Comparable maximumValue = maximumNumber.findMaximum(10,20,30,40,50);
+        Assert.assertEquals(50,maximumValue);
+    }
+
+    @Test
+    public void givenThreeFloat_shouldReturnMaximumFloat() {
+        Comparable maximumValue = maximumNumber.findMaximum(10.1,20.1,30.1,40.1,50.1);
+        Assert.assertEquals(50.1, maximumValue);
+    }
+
+    @Test
+    public void givenThreeString_shouldReturnMaximumString() {
+        Comparable maximumValue = maximumNumber.findMaximum("BridgeLabz","Accenture","Capgiminai","Expleo");
+        Assert.assertEquals("Expleo", maximumValue);
+    }
 }
