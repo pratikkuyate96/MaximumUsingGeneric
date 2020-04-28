8200d776a5edb10ec5dd72f9858d641ce59c288d
pratikkuyate96 <62277306+pratikkuyate96@users.noreply.github.com>
2020-04-28 16:42:15 +0530

    REFACTOR2: Create generic class and print maximum value

--- src/test/java/com/bl/maxNum/MaximumNumberTest.java
+++ src/test/java/com/bl/maxNum/MaximumNumberTest.java
@@ -57,4 +57,22 @@ public class MaximumNumberTest {
         Assert.assertEquals((String) "Capgiminai",maximum);
     }
 
+    @Test
+    public void givenMoreThanThreeIntegers_shouldReturnMaximumInteger() {
+        Comparable maximumValue = maximumNumber.findMaximum(5, 2, 3, 4, 1);
+        Assert.assertEquals(5, maximumValue);
+    }
+
+    @Test
+    public void givenMoreThanThreeFloats_shouldReturnMaximumFloat() {
+        Comparable maximumValue = maximumNumber.findMaximum(5.1, 2.1, 3.1, 4.1, 1.1);
+        Assert.assertEquals(5.1, maximumValue);
+    }
+
+    @Test
+    public void givenMoreThanThreeStrings_shouldReturnMaximumString() {
+        Comparable maximumValue = maximumNumber.findMaximum("apple", "banana", "peach", "mango", "watermelon");
+        Assert.assertEquals("watermelon", maximumValue);
+    }
+
 }
