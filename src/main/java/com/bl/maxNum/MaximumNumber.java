d50162a167f00131e778875a30513ef284c60f0c
pratikkuyate96 <62277306+pratikkuyate96@users.noreply.github.com>
2020-04-28 16:34:22 +0530

    REFACTOR1: Add generic method

--- src/main/java/com/bl/maxNum/MaximumNumber.java
+++ src/main/java/com/bl/maxNum/MaximumNumber.java
@@ -1,43 +1,56 @@
 package com.bl.maxNum;
 
-public class MaximumNumber {
+import java.util.Arrays;
 
-    public int findMaximum(Integer firstNumber,Integer secondNumber,Integer thirdNumber) {
+public class MaximumNumber<E extends Comparable<E>> {
 
-        if (firstNumber.compareTo(secondNumber) >= 0 && firstNumber.compareTo(thirdNumber) >= 0) {
-            return firstNumber;
-        }
-        else if (secondNumber.compareTo(firstNumber) >= 0 && secondNumber.compareTo(thirdNumber) >= 0) {
-            return secondNumber;
-        }
-        else {
-            return thirdNumber;
-        }
+    private E firstValue;
+    private E secondValue;
+    private E thirdValue;
+
+    public MaximumNumber() {
     }
 
-    public float findMaximum(Float firstNumber,Float secondNumber,Float thirdNumber) {
-
-        if (firstNumber.compareTo(secondNumber) >= 0 && firstNumber.compareTo(thirdNumber) >= 0) {
-            return firstNumber;
-        }
-        else if (secondNumber.compareTo(firstNumber) >= 0 && secondNumber.compareTo(thirdNumber) >= 0) {
-            return secondNumber;
-        }
-        else {
-            return thirdNumber;
-        }
+    public E findMaximum() {
+        return findMaximum(firstValue, secondValue, thirdValue);
     }
 
-    public String findMaximum(String firstWord,String secondWord,String thirdWord) {
+    public static <V extends Comparable<V>> V findMaximum(V... valuesArray) {
 
-        if (firstWord.compareTo(secondWord) >= 0 && firstWord.compareTo(thirdWord) >= 0) {
-            return firstWord;
+            Arrays.sort(valuesArray);
+            V maximumValue = valuesArray[valuesArray.length-1];
+            printMaximumValue(maximumValue);
+            return valuesArray[valuesArray.length-1];
         }
-        else if (secondWord.compareTo(firstWord) >= 0 && secondWord.compareTo(thirdWord) >= 0) {
-            return secondWord;
-        }
-        else {
-            return thirdWord;
-        }
+
+    private static <E> void printMaximumValue(E maximumValue) {
+        System.out.println("Maximum value: " + maximumValue);
     }
 }
+    //
+//    public float findMaximum(Float firstNumber,Float secondNumber,Float thirdNumber) {
+//
+//        if (firstNumber.compareTo(secondNumber) >= 0 && firstNumber.compareTo(thirdNumber) >= 0) {
+//            return firstNumber;
+//        }
+//        else if (secondNumber.compareTo(firstNumber) >= 0 && secondNumber.compareTo(thirdNumber) >= 0) {
+//            return secondNumber;
+//        }
+//        else {
+//            return thirdNumber;
+//        }
+//    }
+//
+//    public String findMaximum(String firstWord,String secondWord,String thirdWord) {
+//
+//        if (firstWord.compareTo(secondWord) >= 0 && firstWord.compareTo(thirdWord) >= 0) {
+//            return firstWord;
+//        }
+//        else if (secondWord.compareTo(firstWord) >= 0 && secondWord.compareTo(thirdWord) >= 0) {
+//            return secondWord;
+//        }
+//        else {
+//            return thirdWord;
+//        }
+//    }
+//}
