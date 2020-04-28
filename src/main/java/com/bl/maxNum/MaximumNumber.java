a1c8dcc0de500f8eb29d7a94ea2fa3e841d77ee7
pratikkuyate96 <62277306+pratikkuyate96@users.noreply.github.com>
2020-04-28 11:33:21 +0530

    ADDED: To find maximum number of float value

--- src/main/java/com/bl/maxNum/MaximumNumber.java
+++ src/main/java/com/bl/maxNum/MaximumNumber.java
@@ -14,4 +14,17 @@ public class MaximumNumber {
             return thirdNumber;
         }
     }
+
+    public float findMaximum(Float firstNumber,Float secondNumber,Float thirdNumber) {
+
+        if (firstNumber.compareTo(secondNumber) >= 0 && firstNumber.compareTo(thirdNumber) >= 0) {
+            return firstNumber;
+        }
+        else if (secondNumber.compareTo(firstNumber) >= 0 && secondNumber.compareTo(thirdNumber) >= 0) {
+            return secondNumber;
+        }
+        else {
+            return thirdNumber;
+        }
+    }
 }
--- src/test/java/com/bl/maxNum/MaximumNumberTest.java
+++ src/test/java/com/bl/maxNum/MaximumNumberTest.java
@@ -23,4 +23,23 @@ public class MaximumNumberTest {
         Comparable maximum =maximumNumber.findMaximum(10,20,30);
         Assert.assertEquals(30,maximum);
     }
+
+
+    @Test
+    public void givenThreeFloat_findMaximum_atFirstPosition() {
+        Comparable maximum=maximumNumber.findMaximum(30.1f,20.1f,10.2f);
+        Assert.assertEquals(30.1f,maximum);
+    }
+
+    @Test
+    public void givenThreeFloat_findMaximum_atSecondPosition() {
+        Comparable maximum=maximumNumber.findMaximum(10.2f,30.1f,20.1f);
+        Assert.assertEquals(30.1f,maximum);
+    }
+    
+    @Test
+    public void givenThreeFloat_findMaximum_atThirdPosition() {
+        Comparable maximum =maximumNumber.findMaximum(10.1f,20.1f,30.1f);
+        Assert.assertEquals(30.1f,maximum);
+    }
 }
